ea81cd850846f172538bb6af8947bb1987a021fb
davidfyren <davidlande@getacademy.no>
2023-02-02 14:42:34 +0100

    Ferdig
    Vi er nå ferdige med oppgaven (mest sannsynligvis)

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 let hunger = 100;
 let cleanliness = 100;
 let mood = 100;
-let myInterval = setInterval(subtractOne, 200);
-let game = 'running';
+let myInterval = setInterval(subtractOne, 500);
+let running = true;
 
 // View - det vi ser/html
 
@@ -17,17 +17,18 @@ function updateView() {
   <div id="progressBarKontainer" class="progressBarKontainer">
   <div>
   <progress id="feedingBar" class="feedingBar" value='${hunger}' max="100"></progress> <br>
-  <button class='knapp' onclick="feed()">feed da wabbit</button> 
+  <button class='knapp' onclick="feed()" >feed da wabbit</button> 
   </div>
   <div>
   <progress id="cleaningBar" class="cleaningBar"value='${cleanliness}' max="100"></progress> <br>
-  <button class='knapp' onclick="clean()">clean da wabbit</button>
+  <button class='knapp' onclick="clean()" >clean da wabbit</button>
   </div>
   <div>
   <progress id="moodBar" class="moodBar"value='${mood}' max="100"></progress> <br>
-  <button class='knapp' onclick="play()">play with da wabbit</button>
+  <button class='knapp' onclick="play()" >play with da wabbit</button>
   </div>
   </div>
+  <p id="output"></p>
    
     `;
 
@@ -47,26 +48,35 @@ Lage en funskjon for at kjæledyr-verdiene skal gå nedover
 
 
 function feed() {
-  hunger = hunger + 10;
-  if (hunger > 100) {
-    hunger = 100;
+  if (running == true){ 
+    hunger = hunger + 10;
+    if (hunger > 100) {
+      hunger = 100;
+    }
   }
+ 
   updateView();
 }
 
 function clean() {
-  cleanliness = cleanliness + 10;
-  if (cleanliness > 100) {
-    cleanliness = 100;
+  if (running == true){
+    cleanliness = cleanliness + 10;
+    if (cleanliness > 100) {
+      cleanliness = 100;
+    }
   }
+  
   updateView();
 }
 
 function play() {
-  mood = mood + 10;
-  if (mood > 100) {
-    mood = 100;
+  if (running == true){
+    mood = mood + 10;
+    if (mood > 100) {
+      mood = 100;
+    }
   }
+  
   updateView();
 }
 
@@ -81,11 +91,20 @@ function subtractOne() {
 function gameOver() {
   if (cleanliness == 0 || hunger == 0 || mood == 0) {
     clearInterval(myInterval);
-    game = 'over'
+    running = false;
     console.log("heihei")
+    alert('Game over');
   }
+  
   updateView();
 }
 
 
+//ting vi kan gjøre videre i tamagotchi: 
 
+/*
+- få en tekst over hver bar, f.eks. hunger.
+
+- Legge til prompt for å 
+
+*/
